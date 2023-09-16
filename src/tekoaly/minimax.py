c41fbcfea69a3d26c49a9e371b8950f36c49cc4e
Emilia Kupsanen <92013457+emlyy@users.noreply.github.com>
2023-09-16 19:21:20 +0300

    minimax.py

---
+++ src/tekoaly/minimax.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def minimax(pelilauta, syvyys, ai_vuoro):
+    """Minimax algoritmi. Käy siirrot läpi ja valitsee parhaan vaihtoehdon.
+
+    Args:
+        pelilauta (_type_): Matriisi pelilaudan tilanteesta.
+        syvyys (int): Puun syvyys.
+        ai_vuoro (boolean): Kertoo onko max/ai vuoro.
+
+    Returns:
+        _type_: Palauttaa heuristisen arvon tai valitun siirron.
+    """
+    if syvyys == 0 or taynna() or tarkista_voitto():
+        # laske arvo
+        return arvo
+    if ai_vuoro:
+        arvo = -np.inf
+        for siirto in range(7):
+            # lisää siirto pelilaudalle
+            arvo = max(arvo, minimax(pelilauta, syvyys-1, False))
+        return arvo
+    arvo = np.inf
+    for siirto in range(7):
+        # lisää siirto pelilaudalle
+        arvo = min(arvo, minimax(pelilauta, syvyys-1, True))
+    return arvo
